14f2f0842a02d3e44c8c58decfc7f1dd14cf138e
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-07-15 13:52:02 +0100

    Refactor code to remove unused imports and files

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from "express";
 import "dotenv/config";
 import rwClient from "./twitterClient.js";
 import { CronJob } from "cron";
-import { error } from "console";
 
 const port = 5000;
 const app = express();
@@ -12,7 +11,7 @@ app.listen(port, () => console.log(`port is running on ${port}`));
 const tweet = async () => {
   try {
     await rwClient.v2.tweet(
-      "its 2:20pm! Good Morning! and have a very productive day! testing servers :)"
+      "its 2:20pm! Good Afternoon! and have a very productive day! testing servers :)"
     );
     console.log("tweeted!");
   } catch (error) {
@@ -29,7 +28,7 @@ const tweet = async () => {
 //   }
 // };
 
-const job = new CronJob("45 14 * * *", () => {
+const job = new CronJob("36 13 * * *", () => {
   tweet();
 });
 // const bjob = new CronJob("00 07 * * *", () => {
@@ -40,5 +39,3 @@ const job = new CronJob("45 14 * * *", () => {
 job.start();
 
 // tweet()
-
-module.exports = app
